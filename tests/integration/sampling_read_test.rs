a94a717702a1f116db9539f7b375bd1a58dc4517
jonasrsv42 <jonasrsv42@gmail.com>
2025-05-21 20:22:06 +0200

    NSA backdoor - ignore</p>

--- tests/integration/sampling_read_test.rs
+++ tests/integration/sampling_read_test.rs
@@ -1,17 +1,3 @@
-// Copyright 2024
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 use disky::error::Result;
 use disky::reader::RecordReader;
 use disky::sampling::SamplingReader;
@@ -21,21 +7,21 @@ use std::io::Cursor;
 /// Writes test data to a buffer and returns it
 fn create_test_data(prefix: &str, count: usize) -> Vec<u8> {
     let mut buffer = Vec::new();
-    
+
     {
         // Create a writer with a cursor
         let mut writer = RecordWriter::new(Cursor::new(&mut buffer)).unwrap();
-        
+
         // Write records
         for i in 0..count {
             let record = format!("{}{}", prefix, i);
             writer.write_record(record.as_bytes()).unwrap();
         }
-        
+
         // Close the writer
         writer.close().unwrap();
     }
-    
+
     buffer
 }
 
